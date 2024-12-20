88e9c092d92e4a5402b0487d6bbeea6780322442
Adrien <adrien.costandi@gmail.com>
2024-12-20 13:03:55 +0100

    darkmode fonctionne dans localStorage

--- src/chat-websocket-front/src/App.vue
+++ src/chat-websocket-front/src/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
-import Button from 'primevue/button';
+import Button from 'primevue/button'
 import SplitterPanel from 'primevue/splitterpanel'
 import Splitter from 'primevue/splitter'
 import ChatList from '@/components/ChatList.vue'
 import type { ChatPreview } from '@/services/ChatService.ts'
+import { useDarkModeStore } from '@/stores/darkMode'
+
+const darkModeStore = useDarkModeStore()
+
+if (darkModeStore.darkMode) {
+  document.documentElement.classList.toggle('my-app-dark')
+}
 
 function toggleDarkMode() {
+  console.log(darkModeStore.darkMode)
+  darkModeStore.toggleDarkMode()
   document.documentElement.classList.toggle('my-app-dark')
 }
 
@@ -74,16 +83,51 @@ const chats: ChatPreview[] = [
 </script>
 
 <template>
-  <Button @click="toggleDarkMode()" label="Dark Mode"></Button>
+  <div style="height: 100vh">
+    <header>
+      <h2>Chat</h2>
+      <Button class="dark-mode-toggle" @click="toggleDarkMode()">
+        <i :class="{ 'dark-mode': darkModeStore.darkMode, 'light-mode': !darkModeStore.darkMode }" />
+      </Button>
+    </header>
+    <main>
+      <Splitter style="height: 100%">
+        <SplitterPanel class="flex items-center justify-center" :size="20" :minSize="10">
+          <ChatList :chats="chats" />
+        </SplitterPanel>
+        <SplitterPanel class="flex items-center justify-center" :size="80" :minSize="50">
+          <RouterView />
+        </SplitterPanel>
+      </Splitter>
+    </main>
+  </div>
 
-  <main>
-    <Splitter style="height: 93vh">
-      <SplitterPanel class="flex items-center justify-center" :size="20" :minSize="10">
-        <ChatList :chats="chats" />
-      </SplitterPanel>
-      <SplitterPanel class="flex items-center justify-center" :size="80" :minSize="50">
-        <RouterView />
-      </SplitterPanel>
-    </Splitter>
-  </main>
 </template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  height: var(--header-size);
+  padding: 4px;
+  background-color: var(--p-content-background);
+}
+
+header h2 {
+  flex: 1;
+  text-align: center;
+}
+
+main {
+  height: calc(100vh - var(--header-size));
+}
+
+.dark-mode::after {
+  content: url('https://api.iconify.design/emojione/smiling-face.svg');
+}
+
+.light-mode::after {
+  content: url('https://api.iconify.design/emojione/smiling-face-with-sunglasses.svg');
+}
+
+</style>
---
+++ src/chat-websocket-front/src/stores/darkMode.ts
@@ -0,0 +1,14 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useDarkModeStore = defineStore('darkMode', () => {
+    const darkMode = ref((localStorage.getItem('darkMode') === "true"))
+
+    function toggleDarkMode() {
+      darkMode.value = !darkMode.value
+      localStorage.setItem('darkMode', darkMode.value + "")
+    }
+
+    return { darkMode, toggleDarkMode }
+  }
+)
